76d9af536fdb42c8c2c18089882c9e8b6ca5be23
akashyadavtuhh <akash.yadav@tuhh.de>
2024-01-04 14:40:49 -0800

    [update]: added bottom nav component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import BottomNavigation, {
+  navOptions,
+} from "./components/common/BottomNavigation";
+
 export default function Home() {
   return (
     <>
       <h1>nBud</h1>
+      <BottomNavigation options={navOptions} />
     </>
   );
 }
